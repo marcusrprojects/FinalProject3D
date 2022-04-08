fde1e512873a447fdc6d25b7c0078b094a8362dc
Collin Presser <pressercc@gmail.com>
2022-04-08 10:05:42 +0200

    Created Audio Manager

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,24 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+//Made using this tutorial: https://www.youtube.com/watch?v=6OT43pvUyfY
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0f, 12f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
